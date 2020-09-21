e85385ae839657a22d706b15a5e813a22d75c4fc
matiac <ac.matif@gmail.com>
2020-09-21 19:35:37 -0300

    vista login

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+@extends('plantilla')
+
+@section('contenido')
+
+<div class="h-100 d-flex flex-column justify-content-center bg-light">
+    <h1 class="text-center mb-4"> Software de Gestión de Stock </h1>
+    <div class="d-flex justify-content-center">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header bg-dark text-white">
+                    <h4 class="card-title">Inicio Sesión</h4>
+                </div>
+                <div class="card-body">
+                    <form action="" id="login">
+                        <div class="form-group">
+                            <label for="usuario">Usuario</label>
+                            <input type="text" class="form-control" name="usuario" id="">
+                        </div>
+                        <div class="form-group">
+                            <label for="contraseña">Contraseña</label>
+                            <input type="password" class="form-control" name="contraseña" id="">
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer bg-dark">
+                    <button type="submit" class="btn btn-success float-right" form="login">Ingresar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
